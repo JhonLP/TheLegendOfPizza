7d99e7b8fa2bd8a0c5021d0839c36d171d16c11c
Jhonatan Laguna <JhonLPdev@gmail.com>
2015-07-03 23:43:10 -0700

    Proyecto completo

---
+++ LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using NinjaPrototypeMobile.Resources;
+
+namespace NinjaPrototypeMobile
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
